50bca54e2d10ab061dc1e78714468ca30be90b9d
Amin Saniefar <aminsanaiefar@gmail.com.>
2025-01-03 14:12:53 +0330

    create veiw of order create and connect urls

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect
 from django.views.decorators.http import require_POST
+from django.contrib import messages
+from django.utils.translation import gettext as _
+from django.http import HttpResponseRedirect
 
 from products.models import Product
 from .cart import Cart
@@ -21,13 +24,15 @@ def cart_detail_view(request):
 def add_to_cart_view(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
-    form = AddToCartProductForm(request.POST)
 
+    form = AddToCartProductForm(request.POST)
     if form.is_valid():
         cleaned_data = form.cleaned_data
         quantity = cleaned_data['quantity']
         cart.add(product, quantity, replace_current_quantity=cleaned_data['inplace'])
-
+    else:
+        messages.error(request, _('Invalid Request'))
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
     return redirect('cart:cart-detail')
 
 
--- products/views.py
+++ products/views.py
@@ -15,9 +15,6 @@ class ProductListView(generic.ListView):
     template_name = 'products/products_list.html'
     context_object_name = 'products'
 
-    # def get_queryset(self):
-    #     return Product.objects.filter(active=True)
-
 
 class ProductDetailView(generic.DetailView):
     model = Product
